4da80b60c0355b2076430940fed1f17298040617
Stefan Schlager <zarquon42@gmail.com>
2015-12-01 08:30:03 +0100

    added dwdcv

---
+++ R/dwdcv.r
@@ -0,0 +1,21 @@
+#' leaving one out cross-validation for DWD
+#'
+#' leaving one out cross-validation for DWD
+#' @param x predictor
+#' @param y binary coded variable for classification
+#' @param ... arguments passed to kerndwd and predict.kerndwd
+#' @return vector of cross-validated predictions
+#' @export
+dwdcv <- function(x,y,...) {
+    require(kerndwd)
+    n <- length(y)
+    cvfun <- function(i) {
+        xtmp <- x[-i,,drop=FALSE]
+        ytmp <- y[-i]
+        dwdtmp <- kerndwd(xtmp,ytmp,...)
+        out <- predict.kerndwd(dwdtmp,x=xtmp,newx=x[i,,drop=FALSE],...)
+        return(out)
+    }
+    allout <- unlist(parallel::mclapply(1:n,cvfun))
+}
+    
